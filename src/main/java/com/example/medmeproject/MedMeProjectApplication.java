0c4d52b0dc792d00b9d6dffc66585b566e22c5fd
faroukshtewee <farok95@hotmail.com>
2025-11-21 05:59:26 +0200

    first commit documents and graphs

---
+++ src/main/java/com/example/medmeproject/MedMeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.medmeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedMeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedMeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medmeproject/MedMeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medmeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedMeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
